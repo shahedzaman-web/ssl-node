e64cc6d89c02e591bdba58a1ed173d1f9ad2bb8d
shahedzaman-web <physics.shahedzaman@gmail.com>
2021-10-31 14:34:11 +0600

    7th c

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ app.get('/init', (req, res) => {
         currency: 'BDT',
         tran_id: 'REF123', // use unique tran_id for each api call
         success_url: `${url}/success`,
-        fail_url:  `${url}+/fail`,
-        cancel_url: `${url}+/cancel`,
-        ipn_url: `${url}+/ipn`,
+        fail_url:  `${url}/fail`,
+        cancel_url: `${url}/cancel`,
+        ipn_url: `${url}/ipn`,
         shipping_method: 'Courier',
         product_name: 'Computer.',
         product_category: 'Electronic',
@@ -47,7 +47,7 @@ app.get('/init', (req, res) => {
     sslcz.init(data).then(apiResponse => {
         // Redirect the user to payment gateway
         let GatewayPageURL = apiResponse.GatewayPageURL
-        // res.redirect(GatewayPageURL)
+        //  res.redirect(GatewayPageURL)
         res.send({
             url: GatewayPageURL,
         message: "success"})
@@ -55,9 +55,55 @@ app.get('/init', (req, res) => {
     });
 })
 app.post('/success',(req,res)=>{
-    return res.status(200).json({
-       message: "Payment successful!"
-    })
+    return res.status(200).send(
+    `<html>
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
+  </head>
+    <style>
+      body {
+        text-align: center;
+        padding: 40px 0;
+        background: #EBF0F5;
+      }
+        h1 {
+          color: #88B04B;
+          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+          font-weight: 900;
+          font-size: 40px;
+          margin-bottom: 10px;
+        }
+        p {
+          color: #404F5E;
+          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+          font-size:20px;
+          margin: 0;
+        }
+      i {
+        color: #9ABC66;
+        font-size: 100px;
+        line-height: 200px;
+        margin-left:-15px;
+      }
+      .card {
+        background: white;
+        padding: 60px;
+        border-radius: 4px;
+        box-shadow: 0 2px 3px #C8D0D8;
+        display: inline-block;
+        margin: 0 auto;
+      }
+    </style>
+    <body>
+      <div class="card">
+      <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
+        <i class="checkmark">✓</i>
+      </div>
+        <h1>Success</h1> 
+        <p>We received your purchase request;<br/> we'll be in touch shortly!</p>
+      </div>
+    </body>
+</html>`)
 } )
 //sslcommerz validation 
 
